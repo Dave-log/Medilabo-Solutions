18953717f2cbcb8ba6e4ff69db3b53291466c524
Dave-log <david.garcia45@hotmail.fr>
2024-06-18 15:54:01 +0200

    Project setup

---
+++ patient-service/src/main/java/com/medilabo/patient_service/PatientServiceApplication.java
@@ -0,0 +1,14 @@
+package com.medilabo.patient_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class PatientServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientServiceApplication.class, args);
+	}
+
+}
