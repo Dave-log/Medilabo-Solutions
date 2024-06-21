7327be95fd95012675c8395350d915666fa409e6
Dave-log <david.garcia45@hotmail.fr>
2024-06-21 12:31:04 +0200

    Gitlab CI fix

--- patient-service/src/test/java/com/medilabo/patient_service/PatientServiceApplicationTests.java
+++ patient-service/src/test/java/com/medilabo/patient_service/PatientServiceApplicationTests.java
@@ -3,7 +3,7 @@ package com.medilabo.patient_service;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = {PatientServiceApplication.class})
 class PatientServiceApplicationTests {
 
 	@Test
