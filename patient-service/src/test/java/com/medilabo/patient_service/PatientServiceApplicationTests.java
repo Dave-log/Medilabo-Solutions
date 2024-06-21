580eba8d0a801c32bfcb8153544fd33d141a62cd
Dave-log <david.garcia45@hotmail.fr>
2024-06-21 09:22:36 +0200

    Test without UT

--- patient-service/src/test/java/com/medilabo/patient_service/PatientServiceApplicationTests.java
+++ patient-service/src/test/java/com/medilabo/patient_service/PatientServiceApplicationTests.java
@@ -3,11 +3,11 @@ package com.medilabo.patient_service;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
-//class PatientServiceApplicationTests {
-//
+@SpringBootTest
+class PatientServiceApplicationTests {
+
 //	@Test
 //	void contextLoads() {
 //	}
-//
-//}
+
+}
