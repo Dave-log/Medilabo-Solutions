31a90b37a6ba3d013aef41b39cfbaa8d868f5513
Dave-log <david.garcia45@hotmail.fr>
2024-06-25 11:29:30 +0200

    test gitlab CI with discovery MS

--- discovery-service/src/test/java/com/medilabo/discovery_service/DiscoveryServiceApplicationTests.java
+++ discovery-service/src/test/java/com/medilabo/discovery_service/DiscoveryServiceApplicationTests.java
@@ -2,9 +2,11 @@ package com.medilabo.discovery_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 
 @SpringBootTest
+@ActiveProfiles("test")
 class DiscoveryServiceApplicationTests {
 
 	@Test
