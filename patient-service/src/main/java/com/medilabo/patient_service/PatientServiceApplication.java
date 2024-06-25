6fc620ef757bdeae3c7043e0401b3c689d1db281
Dave-log <david.garcia45@hotmail.fr>
2024-06-25 11:17:59 +0200

    setup discovery microservice with eureka

--- patient-service/src/main/java/com/medilabo/patient_service/PatientServiceApplication.java
+++ patient-service/src/main/java/com/medilabo/patient_service/PatientServiceApplication.java
@@ -2,9 +2,10 @@ package com.medilabo.patient_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class PatientServiceApplication {
 
 	public static void main(String[] args) {
