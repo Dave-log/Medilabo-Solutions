adbbdfd82048b56b5adb877d5290f63cc17728a3
Dave-log <david.garcia45@hotmail.fr>
2024-07-04 11:37:57 +0200

    Implements basic frontend MS

--- frontend-service/src/App.tsx
+++ frontend-service/src/App.tsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import Patient from './Patient';
+
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
+
       <header className="App-header">
         <h1>Application de gestion des patients</h1>
       </header>
+
       <main>
         <Patient />
       </main>
+      
     </div>
   );
+
 }
 
 export default App;
