0937804fab60c5d3d67ac0ecea52ac54a8acd3cf
Dave-log <david.garcia45@hotmail.fr>
2024-07-30 01:11:27 +0200

    Enable connection to mongoDB from notes-service.

--- notes-service/src/main/java/com/medilabo/notes_service/NotesServiceApplication.java
+++ notes-service/src/main/java/com/medilabo/notes_service/NotesServiceApplication.java
@@ -1,15 +1,33 @@
 package com.medilabo.notes_service;
 
+import com.medilabo.notes_service.model.Note;
+import com.medilabo.notes_service.repository.NoteRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+import java.util.List;
+
 @SpringBootApplication
 @EnableDiscoveryClient
-public class NotesServiceApplication {
+public class NotesServiceApplication implements CommandLineRunner {
+
+	private final Logger logger = LoggerFactory.getLogger(NotesServiceApplication.class);
+
+	@Autowired
+	NoteRepository noteRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(NotesServiceApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		List<Note> notes = noteRepository.findAll();
+		notes.forEach(note -> logger.info(note.toString()));
+	}
 }
---
+++ notes-service/src/main/java/com/medilabo/notes_service/model/Note.java
@@ -0,0 +1,20 @@
+package com.medilabo.notes_service.model;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Data
+@Document(collection = "notes")
+public class Note {
+    @Id
+    private String id;
+    @Indexed(unique = true)
+    private String patientId;
+    private String patientName;
+    private String note;
+    private LocalDateTime date = LocalDateTime.now();
+}
