5d75d1c2fa513b9752760f64b23a162b0973e399
Dave-log <david.garcia45@hotmail.fr>
2024-08-08 10:27:22 +0200

    adds openapi for rest apis

---
+++ gateway-service/src/main/java/com/medilabo/gateway_service/configuration/OpenApiConfig.java
@@ -0,0 +1,29 @@
+package com.medilabo.gateway_service.configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Diabetes Report API")
+                        .version("1.0")
+                        .description("This microservice is responsible for assessing the risk of diabetes in patients."));
+    }
+
+//    @Bean
+//    public GroupedOpenApi storeOpenApi() {
+//        String[] paths = {"/medilabo_api/**"};
+//        return GroupedOpenApi.builder()
+//                .group("medilabo_apis")
+//                .pathsToMatch(paths)
+//                .build();
+//    }
+}
